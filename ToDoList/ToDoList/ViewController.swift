33fb51a366223b14d0411712aebb48dc67b0d802
Shweta Shah <shwetushah123@gmail.com>
2019-10-15 17:51:56 +0530

    Made a TODO List  MAC Application in which user can: 1. Add Item in list 2. Make an item important. 3. Delete item from list

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Shweta Shah on 14/10/19.
+//  Copyright © 2019 Agrahyah. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var todoText: NSTextField!
+    @IBOutlet weak var importantCheckBox: NSButton!
+    
+    var appDelegate = NSApplication.shared.delegate as? AppDelegate
+    var todoItems = [ToDoItem]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        getTodoItems()
+        // Do any additional setup after loading the view.
+    }
+
+    func getTodoItems(){
+        
+        if let context =  appDelegate?.persistentContainer.viewContext{
+    
+            do{
+            
+            //Set them to class property
+            todoItems = try context.fetch(ToDoItem.fetchRequest())
+                 
+            }catch{
+                
+            }
+            
+            //update Table View
+            tableView.reloadData()
+        }
+    }
+    
+    @IBAction func addClicked(_ sender: Any) {
+        
+        if todoText.stringValue != "" {
+           if let context =  appDelegate?.persistentContainer.viewContext{
+                
+                //Get the todoitem context
+                let todoItem = ToDoItem(context: context)
+                todoItem.name = todoText.stringValue
+            
+                if(importantCheckBox.state == .off){
+                    todoItem.important = false
+                }else{
+                    todoItem.important = true
+                }
+            
+                //Saving the data
+                appDelegate?.saveAction(nil)
+                
+                // Removing the data from the feild
+                todoText.stringValue = ""
+                importantCheckBox.state = .off
+                
+                getTodoItems()
+            }
+        }
+    }
+    
+}
+
+
+ // MARK: - Table View methods
+extension ViewController : NSTableViewDelegate , NSTableViewDataSource{
+    
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return todoItems.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        if tableColumn?.identifier == NSUserInterfaceItemIdentifier(rawValue: "ImpIndetifier"){
+            if let cell =  tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "importantCell"), owner: self) as? NSTableCellView{
+             let todoItem = todoItems[row]
+            if todoItem.important == false{
+                 cell.textField?.stringValue = ""
+            }else{
+                 cell.textField?.stringValue = "❗️"
+            }
+                 return cell
+            }
+            
+        }else{
+           if let cell =  tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "ToDoCell"), owner: self) as? NSTableCellView{
+                 let todoItem = todoItems[row]
+                if let name = todoItem.name{
+                cell.textField?.stringValue = name
+                }else{
+                cell.textField?.stringValue = ""
+                }
+                
+                return cell
+            }
+        }
+        
+        return nil
+   
+        
+    }
+    
+}
+
